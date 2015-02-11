03bb0c580afce58cad10ea5c245f06258e46b996
Ruben de Vries <ruben@rubensayshi.com>
2015-02-11 11:49:31 +0100

    initial commit with code from hackathon

---
+++ README.md
@@ -0,0 +1,22 @@
+Slack TipBot
+============
+Slack TipBot using BlockTrail API.
+
+ToDo
+----
+ - secure passwords
+ - better way of dealing with errors
+ - transaction history
+ - withdraw
+ - ignore inactive users
+ - test if new users are dealt with properly
+ - min tip amount (higher than base fee)
+
+Run
+---
+ - Create a blocktrail account and an API key, copy the API key and secret
+ - Add a bot at https://my.slack.com/services/new/bot and copy the token
+
+```sh
+TIPBOT_SLACK_TOKEN="<YOURTOKEN>" TIPBOT_BLOCKTRAIL_APIKEY="<YOUR_APIKEY>" TIPBOT_BLOCKTRAIL_APISECRET="<YOUR_APISECRET>" npm start
+```
---
+++ bot.js
@@ -0,0 +1,75 @@
+var _ = require('lodash');
+var debug = require('debug')('tipbot:bot');
+var Slack = require('slack-client');
+var TipBot = require('./lib/tipbot');
+
+
+
+var SLACK_TOKEN = process.env.TIPBOT_SLACK_TOKEN,
+	BLOCKTRAIL_APIKEY = process.env.TIPBOT_BLOCKTRAIL_APIKEY,
+	BLOCKTRAIL_APISECRET = process.env.TIPBOT_BLOCKTRAIL_APISECRET,
+	TESTNET = true,
+    AUTO_RECONNECT = true,
+	OPTIONS = {ALL_BALANCES: true, DEMAND: true};
+
+/**
+ * find a DM channel object by userID
+ *
+ * @param userId
+ * @returns {*}
+ */
+Slack.prototype.getDMByUserId = function(userId) {
+	return _.find(this.dms, {user: userId});
+};
+
+var slack = new Slack(SLACK_TOKEN, AUTO_RECONNECT, /* AUTO_MARK */ true);
+var tipbot = new TipBot(slack, BLOCKTRAIL_APIKEY, BLOCKTRAIL_APISECRET, TESTNET, OPTIONS);
+
+slack.on('open', function() {
+	var channels = [],
+	    groups = [];
+
+	_.each(slack.channels, function(channel, key) {
+		if (channel.is_member) {
+			channels.push('#' + channel.name);
+		}
+	});
+
+	_.each(slack.groups, function(group, key) {
+		if (group.is_open && !group.is_archived) {
+			groups.push(group.name);
+		}
+	});
+
+	debug('Connected to Slack. You are @%s of %s', slack.self.name, slack.team.name);
+	debug('You are in (channels): %s', channels.join(', '));
+	debug('As well as (groups): %s', groups.join(', '));
+
+	// init the tipbot
+	tipbot.init();
+});
+
+slack.on('message', function(message) {
+	debug('MESSAGE', message.type, message.channel, message.user, message.text);
+
+	var type = message.type,
+	    channel = slack.getChannelGroupOrDMByID(message.channel),
+	    member = slack.getUserByID(message.user);
+
+	// Respond to messages with the reverse of the text received.
+	if (type === 'message') {
+		// random stuff we can safely ignore
+		if (!message.text || !member) {
+			return;
+		}
+
+		// let tipbot handle the message
+		tipbot.onMessage(channel, member, message.text);
+	}
+});
+
+slack.on('error', function(error) {
+	debug('Error: %s', error);
+});
+
+slack.login();
