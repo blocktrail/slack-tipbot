def845d16842122016e48ddd9666736c3a9a6f0c
Ruben de Vries <ruben@rubensayshi.com>
2015-03-06 11:27:41 +0100

    log socketURL

--- bot.js
+++ bot.js
@@ -49,7 +49,8 @@ slack.on('open', function() {
         }
     });
 
-    debug('tipbot:bot')('Connected to Slack. You are <@%s:%s> of %s', slack.self.id, slack.self.name, slack.team.name);
+    debug('tipbot:bot')('Connected to Slack, SocketURL: %s', slack.socketUrl);
+    debug('tipbot:bot')('You are <@%s:%s> of %s', slack.self.id, slack.self.name, slack.team.name);
     debug('tipbot:bot')('You are in (channels): %s', channels.join(', '));
     debug('tipbot:bot')('As well as (groups): %s', groups.join(', '));
 
